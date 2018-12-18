42cd891f3bc6a184e490a7c98a054661e2c0407a
Koretsky Mikhail <koretsky97@gmail.com>
2018-12-19 01:49:22 +0300

    bash script fix

--- restart_containers.sh
+++ restart_containers.sh
@@ -1,15 +1,15 @@
-  sudo docker pull 'olegschwann/authorization_server':latest && \
-  sudo docker kill 'authorization' && \
+  sudo docker pull olegschwann/authorization_server:latest &&
+  sudo docker kill authorization
   sudo docker run \
   --name 'authorization' \
   --network 'rpsarena-net' \
   --volume "/var/www/media/images":"/var/www/media/images" \
   --detach \
-  --rm olegschwann/authorization_server':latest; &&\
-  sudo docker pull 'olegschwann/game_server':latest && \
-  sudo docker kill 'game' && \
+  --rm olegschwann/authorization_server:latest &&
+  sudo docker pull olegschwann/game_server:latest && 
+  sudo docker kill game
   sudo docker run \
   --name 'game' \
   --network 'rpsarena-net' \
   --detach \
-  --rm olegschwann/game_server':latest;
+  --rm olegschwann/game_server:latest 
