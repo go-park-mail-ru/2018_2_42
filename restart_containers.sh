9311b0ed82622e2354cae74328ce77eb92a82bc3
Koretsky Mikhail <koretsky97@gmail.com>
2018-12-19 20:46:02 +0300

    travis lint

--- restart_containers.sh
+++ restart_containers.sh
@@ -1,3 +1,6 @@
+  go get github.com/fzipp/gocyclo &&
+  go build -o "${GOPATH}/bin/gocyclo" 'github.com/fzipp/gocyclo' &&
+  find -name *.go -exec "${GOPATH}/bin/gocyclo" -over 25  {} + &&
   sudo docker pull olegschwann/authorization_server:latest &&
   sudo docker kill authorization
   sudo docker run \
